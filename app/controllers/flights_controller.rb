06b31568890924584d0575e452592473d9401e48
toponeoftopone <nooblingis@gmail.com>
2016-04-05 16:01:26 +0800

    making flight selection form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,16 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @start_datetimes = Flight.all.map { |flight| flight.start_datetime.to_date }.uniq
+    @start_datetimes = Flight.all.map { |flight| [flight.start_datetime.to_date, flight.start_datetime] }.uniq
     @airport_ids = Airport.all.map { |airport| [airport.code, airport.id] }
     @num_passengers = (1..4).to_a
 
     # searching
-    @flights = Flight.where(flight_search_params)
+    @flights = Flight.where(flight_search_params) unless params[:flight_search].nil?
   end
 
   private
   def flight_search_params
-    params.require(:flight_search).permit(:start_datetimes, :to_airport_id, :from_airport_id, :num_passengers) unless params[:flight_search].nil?
+    params.require(:flight_search).permit(:start_datetime, :to_airport_id, :from_airport_id)
   end
 
 end
