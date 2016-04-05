48350d31bb38723a0bff323d08239820fb255c8f
toponeoftopone <nooblingis@gmail.com>
2016-04-05 17:31:14 +0800

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@
   Airport.create(code: ('A'..'Z').to_a[n..n+2].join)
 end
 
-1.times do |n|
+100.times do |n|
   Airport.all.each do |from_airport|
     Airport.all.each do |to_airport|
       Flight.create(from_airport_id: from_airport.id, to_airport_id: to_airport.id,
-                    start_datetime: rand.weeks.from_now, flight_duration: rand.days)
+                    start_datetime: (rand * 7).days.from_now, flight_duration: rand.days) unless from_airport == to_airport
 
     end
   end
