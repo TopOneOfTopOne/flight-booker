ec14d9dff7209a003434b1f4e97f64e6efe53048
toponeoftopone <nooblingis@gmail.com>
2016-04-06 10:02:01 +0800

    Refactored + corrected code

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,14 +4,13 @@ class FlightsController < ApplicationController
     @airport_ids = Airport.all.map { |airport| [airport.code, airport.id] }
     @num_passengers = (1..4).to_a
 
-    # searching
-    @flights = Flight.search(flight_search_params) unless params[:flight_search].nil?
-    @selected_num_passengers = params[:flight_search][:num_passengers]
+    @searched_for = flight_search_params
+    @info = {flights: Flight.search(flight_search_params), num_passengers: params[:flight_search][:num_passengers]} unless params[:flight_search].nil?
   end
 
   private
   def flight_search_params
-    params.require(:flight_search).permit(:start_datetime, :to_airport_id, :from_airport_id)
+    params.require(:flight_search).permit(:start_datetime, :to_airport_id, :from_airport_id, :num_passengers)
   end
 
 end
