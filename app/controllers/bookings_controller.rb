ed80f06a3b3b0c61c5ac610a0b5709b2aa025eea
toponeoftopone <nooblingis@gmail.com>
2016-04-06 12:04:08 +0800

    complete bookings#new + view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,8 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new(flight_id: params[:booking][:flight])
+    @num_passengers = params[:booking][:num_passengers]
+    @num_passengers.to_i.times { @booking.passengers.build }
   end
 
 end
