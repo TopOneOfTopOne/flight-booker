61b001fc28f7f8df79468dd7f921dfe929ac7203
toponeoftopone <nooblingis@gmail.com>
2016-04-06 14:37:27 +0800

    finish

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,26 @@
 class BookingsController < ApplicationController
 
+  def index
+    @bookings = Booking.all
+  end
+
   def new
     @booking = Booking.new(flight_id: params[:booking][:flight])
     @num_passengers = params[:booking][:num_passengers]
     @num_passengers.to_i.times { @booking.passengers.build }
   end
 
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to all_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'flights' => 'flights#index'
   get 'bookings' => 'bookings#new'
+  get 'all' => 'bookings#index'
+  post 'bookings' => 'bookings#create'
   root 'flights#index'
 end
